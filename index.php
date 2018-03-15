010913835a3019e33333dd9e09a2026257027b25
eslarian <eslarian@github.com>
2018-03-15 11:35:42 +0000

    The basic file/folder structure is in place. Not sure why my css file isn't registering, probably something to do with hoow I included it

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+		
+	<head>
+		<meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title> Emile Antognetti </title>
+	<head>
+	<body>
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">Emile Antognetti</a>
+		    </div>
+		    <ul class="nav navbar-nav">
+		      <li class="active"><a href="#">Home</a></li>
+		      <li><a href="#">My Qualifications</a></li>
+		      <li><a href="#">Public Repositories</a></li>
+		      <li><a href="#">Resume</a></li>
+		    </ul>
+		  </div>
+		</nav>
+
+
+
+
+	</body>
+
+
+
+
+
+<!-- Bootstrap Imports -->
+ <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+
+<!-- CSS Imports -->
+<link rel="stylesheet" href="./css/"
+
+</html>
