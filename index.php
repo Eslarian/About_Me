125877397caa7b44a5055515bb7225caf3c8efd9
eslarian <eslarian@github.com>
2018-03-21 10:48:48 +0000

    Added scrollspy,need to find better solution for qualification display

--- index.php
+++ index.php
@@ -3,43 +3,61 @@
 		
 	<head>
 		<meta charset="utf-8">
-		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootstrap Imports -->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+
+		<!-- CSS Imports -->
+		<link rel="stylesheet" href="./css/index.css">
+
+
 		<title> Emile Antognetti </title>
 	<head>
 
-	<body>
+	<body data-spy="scroll" data-target=".navbar" data-offset="80">
 		<!-- Using a bootstrap navbar -->
-		<nav class="navbar navbar-inverse">
+		<nav class="navbar navbar-inverse navbar-fixed-top ">
 		  <div class="container-fluid">
 		    <div class="navbar-header">
 		      <a class="navbar-brand" href="#">About Me</a>
 		    </div>
 		    <ul class="nav navbar-nav">
-		      <li class="active"><a href="#">Profile</a></li>
-		      <li><a href="#">My Qualifications</a></li>
-		      <li><a href="#">Public Repositories</a></li>
-		      <li><a href="#">Resume</a></li>
+		      <li class="active"><a href="#Prof">Profile</a></li>
+		      <li><a href="#Qual">My Qualifications</a></li>
+		      <li><a href="#Repo">Public Repositories</a></li>
+		      <li><a href="#Res">Resume</a></li>
 		    </ul>
 		  </div>
 		</nav>
 
 		<!-- Profile Section: Short description  -->
-		<div class="container-fluid Profile text-center">
+		<div id="Prof" class="container-fluid Profile text-center">
 			<h1> Who Am I?</h1>
 			<img src="https://goo.gl/bMdZko" class="img-circle" alt="me">
 			<h3> My name is Emile Amedeo Antognetti.</h3>
 			<h3> I'm a software developer and recent graduate of RMIT University </h3>
 		</div>
 
-		<div class="container-fluid MyQual">
+		<div id="Qual" class="container-fluid MyQual">
 			<h1>My Qualifications</h1>
+			<div class="row">	
+				<div class="col-sm-2">
+					<img class="img-responsive" src="./assets/document-1.png"></img>
+				</div>
+			</div>
 		</div>
 
-		<div class="container-fluid PubRepo">
+		<div id="Repo" class="container-fluid PubRepo text-right">
 			<h1>Public Repositories</h1>
 		</div>
 
-		<div class="container-fluid Resume">
+		<div id="Res" class="container-fluid Resume">
 			<h1>Resume</h1>
 		</div>
 
@@ -51,15 +69,6 @@
 
 
 
-<!-- Bootstrap Imports -->
- <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 
-<!-- CSS Imports -->
-<link rel="stylesheet" href="./css/index.css">
 
 </html>
